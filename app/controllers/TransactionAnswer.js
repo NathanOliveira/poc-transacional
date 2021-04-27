a940a4ed0a40289b968e908e7732be215a67a940
Nathan Justino Oliveira <nathan.justino.oliveira@everis.nttdata.com>
2021-04-27 16:39:50 -0300

    adicionaro metodo get user

--- app/controllers/TransactionAnswer.js
+++ app/controllers/TransactionAnswer.js
@@ -1,11 +1,45 @@
+function getUser(req, resp) {
+
+    var answer = req.body.answer;
+
+    var user = {
+        "clientInfo": {
+            "name": "Nathan Oliveira",
+            "id": 6722,
+            "birthdate": "1991-03-28",
+            "document": "87237236722"
+        }
+    };
+
+    var response = {
+        openContext: req.body.openContext,
+        visibleContext: {
+            ...req.body.visibleContext,
+            user
+        },
+        hiddenContext: req.body.hiddenContext,
+        answer
+    }
+
+    resp.status(200).send(response);
+}
+
 function getPhones(req, resp) {
 
     var answer = req.body.answer;
 
-    var buttons = [
-        {name: "(11) 1234-5678", value: "C:123456"},
-        {name: "(11) 1111-1111", value: "C:123456"},
-        {name: "(11) 2222-2222", value: "C:123456"}
+    var buttons = [{
+            name: "(11) 1234-5678",
+            value: "C:123456"
+        },
+        {
+            name: "(11) 1111-1111",
+            value: "C:123456"
+        },
+        {
+            name: "(11) 2222-2222",
+            value: "C:123456"
+        }
     ]
     answer.content.buttons = buttons;
 
@@ -18,14 +52,23 @@ function getPhones(req, resp) {
 
     resp.status(200).send(response);
 }
+
 function getProduct(req, resp) {
 
     var answer = req.body.answer;
 
-    var buttons = [
-        {name: "Conta Corrente", value: "C:123456"},
-        {name: "Poupança", value: "C:123456"},
-        {name: "Carteira de investimentos", value: "C:123456"}
+    var buttons = [{
+            name: "Conta Corrente",
+            value: "C:123456"
+        },
+        {
+            name: "Poupança",
+            value: "C:123456"
+        },
+        {
+            name: "Carteira de investimentos",
+            value: "C:123456"
+        }
     ]
     answer.content.buttons = buttons;
 
@@ -38,13 +81,14 @@ function getProduct(req, resp) {
 
     resp.status(200).send(response);
 }
-function replace(req,resp){
+
+function replace(req, resp) {
     var answerText = req.body.answer.content.content;
-    for(var parameter in req.query){
-        answerText = answerText.replace(`__${parameter}__`,req.query[parameter]);
+    for (var parameter in req.query) {
+        answerText = answerText.replace(`__${parameter}__`, req.query[parameter]);
     }
     var answer = req.body.answer;
-    answer.content.content = answerText; 
+    answer.content.content = answerText;
     var response = {
         openContext: req.body.openContext,
         visibleContext: req.body.visibleContext,
@@ -57,5 +101,6 @@ function replace(req,resp){
 module.exports = {
     getPhones,
     getProduct,
-    replace
+    replace,
+    getUser
 };
--- app/routes/TransactionAnswer.js
+++ app/routes/TransactionAnswer.js
@@ -1,16 +1,20 @@
 const controller = require('../controllers/TransactionAnswer');
-module.exports = (app) =>{
+module.exports = (app) => {
 
-    app.post('/transactionalCell/setPhones',(req,resp) => {
-        controller.getPhones(req,resp);
+    app.post('/transactionalCell/setPhones', (req, resp) => {
+        controller.getPhones(req, resp);
     });
 
-    app.post('/transactionalCell/setProducts',(req,resp) => {
-        controller.getProduct(req,resp);
+    app.post('/transactionalCell/setProducts', (req, resp) => {
+        controller.getProduct(req, resp);
     });
 
-    app.post('/transactionalCell/replace',(req,resp) => {
-        controller.replace(req,resp);
+    app.post('/transactionalCell/user', (req, resp) => {
+        controller.getUser(req, resp);
+    });
+
+    app.post('/transactionalCell/replace', (req, resp) => {
+        controller.replace(req, resp);
     });
 
 }
